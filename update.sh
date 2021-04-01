651ede421870e38010d57a19566ce3b64fa25cab
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2021-04-01 14:12:27 +0100

    Remove old CSVs

--- update.sh
+++ update.sh
@@ -5,13 +5,14 @@
 
 [ -z "${GITHUB_TOKEN}" ] && echo -e "\033[0;31mPlease define the GITHUB_TOKEN environment variable\033[0m" && exit 1
 
+rm -rf data
 while read line; do
   IFS=';' read -r -a array <<< "${line}"
   repo="${array[0]}"
   stages="${array[1]}"
   dir="data/${repo}"
   mkdir -p "${dir}"
-  echo "${repo} ..."
+  echo "Generating CSV for ${repo} ..."
   if [ -z "${stages}" ]; then
     npx -y @oselvar/connector-github --repo ${repo} --type pullRequests --auth "${GITHUB_TOKEN}"                      > "${dir}/pullRequests.csv"
     npx -y @oselvar/connector-github --repo ${repo} --type issues       --auth "${GITHUB_TOKEN}"                      > "${dir}/issues.csv"
