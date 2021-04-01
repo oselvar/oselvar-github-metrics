557c5cb7f0859fccaf57d1093e9b2e42694bb693
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2021-04-01 15:58:48 +0100

    Fail on error, add license

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 #
 # Updates issues.csv and pullRequests.csv for each repo listed in repositories.txt
 #
-
+set -e
 [ -z "${GITHUB_TOKEN}" ] && echo -e "\033[0;31mPlease define the GITHUB_TOKEN environment variable\033[0m" && exit 1
 
 rm -rf data
@@ -14,10 +14,10 @@ while read line; do
   mkdir -p "${dir}"
   echo "Generating CSV for ${repo} ..."
   if [ -z "${stages}" ]; then
-    npx -y @oselvar/connector-github --repo ${repo} --type pullRequests --auth "${GITHUB_TOKEN}"                      > "${dir}/pullRequests.csv"
-    npx -y @oselvar/connector-github --repo ${repo} --type issues       --auth "${GITHUB_TOKEN}"                      > "${dir}/issues.csv"
+    npx -y @oselvar/connector-github@latest --repo ${repo} --type pullRequests --auth "${GITHUB_TOKEN}"                      > "${dir}/pullRequests.csv"
+    npx -y @oselvar/connector-github@latest --repo ${repo} --type issues       --auth "${GITHUB_TOKEN}"                      > "${dir}/issues.csv"
   else
-    npx -y @oselvar/connector-github --repo ${repo} --type pullRequests --auth "${GITHUB_TOKEN}" --stages "${stages}" > "${dir}/pullRequests.csv"
-    npx -y @oselvar/connector-github --repo ${repo} --type issues       --auth "${GITHUB_TOKEN}" --stages "${stages}" > "${dir}/issues.csv"
+    npx -y @oselvar/connector-github@latest --repo ${repo} --type pullRequests --auth "${GITHUB_TOKEN}" --stages "${stages}" > "${dir}/pullRequests.csv"
+    npx -y @oselvar/connector-github@latest --repo ${repo} --type issues       --auth "${GITHUB_TOKEN}" --stages "${stages}" > "${dir}/issues.csv"
   fi
 done < repositories.txt
